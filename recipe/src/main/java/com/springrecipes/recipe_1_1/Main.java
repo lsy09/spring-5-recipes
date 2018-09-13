9f9059c17e2ce4512cb53c1f7ac6e524c484815f
lsy <suyoun.lee@ysmcorp.com>
2018-09-13 16:41:17 +0900

    ch_01

--- recipe/src/main/java/com/springrecipes/recipe_1_1/Main.java
+++ recipe/src/main/java/com/springrecipes/recipe_1_1/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new GenericXmlApplicationContext("beans.xml");
+        ApplicationContext context = new GenericXmlApplicationContext("beans_1_1.xml");
 
         HelloWorld helloWorld = context.getBean(HelloWorld.class);
         helloWorld.hello();
